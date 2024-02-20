bde03ba8b638eb606e6f6ee7b1097c6674c19272
YArazov <156554298+YArazov@users.noreply.github.com>
2024-02-20 17:08:28 +0800

    collision detection algorithm for circles

--- collisions.js
+++ collisions.js
@@ -24,6 +24,17 @@ export class Collisions {
     }
 
     detectCollisionCircleCircle(o1, o2) {
-        
+        const s1 = o1.shape;
+        const s2 = o2.shape;
+        const dist = s1.position.distanceTo(s2.position);
+        if (dist < s1.radius + s2.radius) { //the circles collide
+            const overlap = s1.radius + s2.radius - dist;
+            const normal = s2.position.clone().subtract(s1.position).normalize();
+            this.collisions.push({
+                collidedPair: [o1, o2],
+                overlap: overlap,
+                normal: normal
+            });
+        }
     }
 }
