6ef23149ab10b48005338b8f5b0c6696f8ce3646
YArazov <156554298+YArazov@users.noreply.github.com>
2024-02-20 16:40:34 +0800

    collisions nested for loop

--- collisions.js
+++ collisions.js
@@ -2,4 +2,28 @@ export class Collisions {
 	constructor() {
 		this.collisions = [];
 	}
+
+    clearCollisions() {
+        this.collisions = [];
+    }
+
+    narrowPhazeDetection(objects) {
+        for(let i=0; i<objects.length; i++) { 
+            for(let j=0; i<objects.length; j++) {
+                if(j>i) {
+                    //detect collisions
+                    //circle collisions
+                    if(objects[i].shape instanceof Circle &&
+                        objects[j].shape instanceof Circle) {
+                        this.detectCollisionCircleCircle(objects[i], objects[j]);    
+                    }
+                    //rectangle collisions
+                }
+            } 
+        }
+    }
+
+    detectCollisionCircleCircle(o1, o2) {
+        
+    }
 }
