25de27b9fa5b60d7e816f509f19d5424731cc798
heheAnpan <106587276+heheAnpan@users.noreply.github.com>
2024-02-29 01:47:56 +0900

    Created collision class

---
+++ collisions.js
@@ -0,0 +1,8 @@
+export class Collisions {
+    //parameter: variable in a method definition; what goes inside ()
+
+	constructor() {
+        //this: keyword that refers to the current instance (a single occurrence of an object) of the class
+		this.collisions = [];
+	}
+}
