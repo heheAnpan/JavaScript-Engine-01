7cbff7be1d6426cd61001f645870756ddd88d496
heheAnpan <106587276+heheAnpan@users.noreply.github.com>
2024-02-29 14:17:05 +0900

    nested for loop

--- collisions.js
+++ collisions.js
@@ -3,6 +3,36 @@ export class Collisions {
 
 	constructor() {
         //this: keyword that refers to the current instance (a single occurrence of an object) of the class
-		this.collisions = [];
+		//class collision will detect all objects and if they detect collision, they will add it to the list
+        this.collisions = [];
 	}
+
+    //we want to start with an empty list of collisions every frame
+    clearCollisions() {
+        this.collisions = [];
+    }
+
+    //check only objects that are close enough that may be colliding
+    //need an input since we're checking circles
+    //logic part
+    narrowPhaseDetection(objects) {
+        for (let i=0; i<objects.length(); i++) {
+            for (let j=0; j<objects.length(); j++) {
+                if(j>i){
+                    //detect collisions
+                    //circle collisions
+                    if(objects[i].shape instanceof Circle && objects[j] instanceof Circle) {
+                        this.detectCollisionCircleCircle(objects[i],objects[j])
+                        }
+                    }
+                    //rectangle collisions
+                }
+            }
+        }
+    }
+
+    //math part
+    detectCollisionCircleCircle(o1, o2) {
+
+    }
 }
